09f6bc986f17e5d003608b23c8d7be628933d188
Gervel Giva <gervelgiva@gmail.com>
2012-08-16 00:32:01 +0800

    tilemap_editor - bug

--- scripts/monmon.js
+++ scripts/monmon.js
@@ -1,25 +1,54 @@
+var SETTINGS = {
+	"MAP" : {
+		"MAX_X" : 3,
+		"MAX_Y" : 3,
+		"TILE_SIZE" : 32
+	},
+	"TILE_TYPE" : {
+		"BACKGROUND" : "BACKGROUND",
+		"SOLID" : true
+	}
+};
+var MAP;
+
+
 $(function(){
+	initMap();
+	var coordinate = "";
+	var map_str = "";
+	var value = "";
+	$("div.row div.column div").live('click', function(){
+		coordinate = ($(this).attr("id")).split(":");
+		value = (prompt("Enter [image]:[layer]")).split(":");
+		MAP[coordinate[0]][coordinate[1]].image = value[0];
+		MAP[coordinate[0]][coordinate[1]].layer = value[1];
+		redrawMap();
+		
+	});
+	/*
 	var KEY = {
 		"w" : 87,
 		"a" : 65,
 		"s" : 83,
 		"d" : 68
 	}
+	
 	var SETTINGS = {
 		"MAP" : {
 			"MAX_X" : 3,
 			"MAX_Y" : 3,
 		}
 	};
+	
 	var player = {
 		x : 1,
 		y : 1
 	};
 	
 	var map = [SETTINGS.MAP.MAX_X][SETTINGS.MAP.MAX_Y];
-	
-    renderMap(map, player);
-    
+	*/
+    //renderMap(map, player);
+    /*
 	$(document).keydown(function(event) {
 		switch(event.keyCode){
 			case KEY.w:
@@ -52,8 +81,7 @@ $(function(){
 			break;
 		}
 	});
-
-
+	*/
 });
 
 
@@ -69,4 +97,51 @@ function renderMap(map, player){
         out += '</div>';
     }
     $('#player_map').html(out);
+}
+
+
+function initMap(){
+	MAP = Array(SETTINGS.MAP.MAX_Y);
+	for( var i = 0 ; i < SETTINGS.MAP.MAX_Y ; i++ ){
+        MAP[i] = Array(SETTINGS.MAP.MAX_X);
+		for( var j = 0 ; j < SETTINGS.MAP.MAX_X ; j++ ){
+			MAP[i][j] = {
+				image	: "empty",
+				layer 	: 0
+				
+			};
+		}
+    }
+	
+	redrawMap();
+	$('#player_map').width(SETTINGS.MAP.TILE_SIZE * SETTINGS.MAP.MAX_X);
+	$('#player_map').height(SETTINGS.MAP.TILE_SIZE * SETTINGS.MAP.MAX_Y);
+	$('#player_map').css("margin", "0 auto");
+}
+
+function redrawMap(){
+	var out = '';
+	var prev_tile_0 = $("#tile_layer_0").attr('src');
+	var prev_tile_1 = $("#tile_layer_1").attr('src');
+	var prev_tile_2 = $("#tile_layer_2").attr('src');
+
+    for( var i = 0 ; i < SETTINGS.MAP.MAX_Y ; i++ ){
+        out += '<div class="row">';
+        for( var j = 0 ; j < SETTINGS.MAP.MAX_X ; j++ ){
+			prev_tile_0 = (prev_tile_0 == null) ? 'images/empty.png' : prev_tile_0;
+			prev_tile_1 = (prev_tile_1 == null) ? 'images/empty.png' : prev_tile_1;
+			prev_tile_2 = (prev_tile_2 == null) ? 'images/empty.png' : prev_tile_2;
+			
+            out += '<div class="column">' +
+            "<div id="+i+":"+j+" class='" + 'empty' + "'>" + 
+			"<img src='" + ((MAP[i][j].layer == 0) ? ('images/' + MAP[i][j].image + '.png') : prev_tile_0) + "' id='tile_layer_0' class='tile_container'></img>" + 
+			"<img src='" + ((MAP[i][j].layer == 1) ? ('images/' + MAP[i][j].image + '.png') : prev_tile_1) + "' id='tile_layer_1' class='tile_container'></img>" + 
+			"<img src='" + ((MAP[i][j].layer == 2) ? ('images/' + MAP[i][j].image + '.png') : prev_tile_2) + "' id='tile_layer_2' class='tile_container'></img>" + 
+			"</div>" +
+            '</div>';
+        }
+        out += '</div>';
+    }
+    $('#player_map').html(out);
+	
 }
