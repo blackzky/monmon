4e813dc6bd530873a34d34991b8a20c2e53d36e5
Gervel Giva <gervelgiva@gmail.com>
2012-08-18 00:42:02 +0800

    Ok cguru

--- scripts/monmon.js
+++ scripts/monmon.js
@@ -1,44 +1,151 @@
+/*
+[row][col]
+----------------
+[[row]:[col],[row]:[col]]
+[[row]:[col],[row]:[col]]
+
+*/
+
 var SETTINGS = {
 	"MAP" : {
-		"MAX_X" : 3,
-		"MAX_Y" : 3,
+		"MAX_COL" : 3,
+		"MAX_ROW" : 3,
 		"TILE_SIZE" : 32
 	},
 	"TILE_TYPE" : {
 		"BACKGROUND" : "BACKGROUND",
 		"SOLID" : true
-	}
+	},
+	"TILES" : [
+		'empty',
+		'grass',
+		'rock',
+		'player'
+	],
+	"LAYERS" : [0, 1, 2],
+	"LAYER_NAME" : ['Background', 'Middle', 'Foreground']
 };
+
+var TILE = {
+	COORDINATES : '',
+	ROW : 0,
+	COL : 0,
+	IMAGE : '',
+	LAYER : 0
+}
+
 var MAP;
 
+function loadTileImageOptions(){
+	var html = "";
+	for(index in SETTINGS.TILES){
+		html += "<option value='" + SETTINGS.TILES[index] + "'>" + SETTINGS.TILES[index] + "</option>";
+	}
+	$("#tile_options #tile_tile").html(html);
+}
+function loadTileLayerOptions(){
+	var html = "";
+	for(index in SETTINGS.LAYERS){
+		html += "<option value='" + SETTINGS.LAYERS[index] + "'>" + SETTINGS.LAYER_NAME[index] + "</option>";
+	}
+	$("#tile_options #tile_layer").html(html);
+}
+
+function tileClicked(tile_coordinates){
+	loadTileImageOptions();
+	loadTileLayerOptions();
+	TILE.COORDINATES = tile_coordinates.split(":");
+	TILE.ROW = TILE.COORDINATES[0];
+	TILE.COL = TILE.COORDINATES[1];
+	$('#tile_options').show();
+	$('#tile_options #tile_info').text('Tile options for [ ROW:' + TILE.ROW + ' ][ COL:' + TILE.COL + ' ]');
+	$("#tile_options #tile_tile").focus();
+}
+function setTileClicked(){
+	TILE.IMAGE = $("#tile_options #tile_tile").val();
+	TILE.LAYER = $("#tile_options #tile_layer").val() * 1;
+	setTile(MAP[TILE.ROW][TILE.COL], TILE.IMAGE, TILE.LAYER);
+	$("#tile_options #tile_tile").val('');
+	$("#tile_options #tile_layer").val('');
+	$('#tile_options').hide();
+	redrawMap();
+}
+
+function exportMap(){
+	var map_str = "";
+	$("#map_options #map_data").val('');
+	for(map_row in MAP){
+		for(tile_col in MAP[map_row]){
+			map_str += "0:" + MAP[map_row][tile_col].tile_layer_0 + ",";
+			map_str += "1:" + MAP[map_row][tile_col].tile_layer_1 + ",";
+			map_str += "2:" + MAP[map_row][tile_col].tile_layer_2 + "|";
+		}
+	}
+	$("#map_options #map_data").val(map_str);
+}
+function importMap(){
+	//alert($("#map_options #map_data").val());
+	var row_str_line = ($("#map_options #map_data").val()).split('|');
+	var col_str_line = "";
+	var tile_str = "";
+	var row_x = 0;
+	/*
+	for(var row_index = 0; row_index < row_str_line.length; row_index++){
+		if (row_str_line[row_index] == '') break;
+		col_str_line = (row_str_line[row_index]).split(',');
+		row_x = row_index % SETTINGS.MAP.MAX_ROW;
+		for(var col_index = 0; col_index < SETTINGS.MAP.MAX_COL; col_index++){
+			tile_str = (col_str_line[col_index]).split(':');
+			setTile(MAP[row_x][col_index], tile_str[1], eval(tile_str[0]));
+		}
+		
+	}
+	*/
+	
+	var col_str = ($("#map_options #map_data").val()).split('|');
+	var layer_str, data_str, row, col, image, layer;
+	var b = 0;
+	for(col_index in col_str){
+		if(col_str[col_index] == '') break;
+		layer_str = col_str[col_index].split(',');
+		for(layer_index in layer_str){
+			data_str = layer_str[layer_index].split(':');
+			row = col_index - b;
+			col = layer_index;
+			layer = data_str[0] * 1;
+			image = data_str[1];
+			
+			alert(row + ":" + col + "|" + layer + ":" + image);
+			//setTile(MAP[row][col], image, layer);
+		}
+		if(col_index%3 == 0) b+= 0; else b++;
+		
+		
+	}
+	redrawMap();
+}
 
 $(function(){
 	initMap();
 	$('#tile_options').hide();
-	var coordinate = "";
-	var image = "";
-	var layer = 0 ;
-	var map_x = 0;
-	var map_y = 0;
 	$("div.row div.column div").live('click', function(){
-		$('#tile_options').show();
-		coordinate = ($(this).attr("id")).split(":");
-		map_x = coordinate[0];
-		map_y = coordinate[1];
-		$('#tile_options #tile_info').text('Tile options for [X:' + map_x + '][Y:' + map_y + ']');
-		$("#tile_options #tile_image").focus();
+		tileClicked($(this).attr("id"));
 	});
 	
 	$("#set_tile_btn").live('click', function(){
-		image = $("#tile_options #tile_image").val();
-		layer = $("#tile_options #tile_layer").val() * 1;
-		setTile(MAP[map_x][map_y], image, layer);
-		$('#tile_options').hide();
-		redrawMap();
+		setTileClicked();
 	});
 	
 	$("#cancel_set_tile_btn").live('click', function(){
 		$('#tile_options').hide();
+	});
+	
+	$("#map_options #import_btn").click(function(){
+		importMap();
+	});
+	
+	$("#map_options #export_btn").click(function(){
+		exportMap();
 	});
 	
 	
@@ -52,8 +159,8 @@ $(function(){
 	
 	var SETTINGS = {
 		"MAP" : {
-			"MAX_X" : 3,
-			"MAX_Y" : 3,
+			"MAX_COL" : 3,
+			"MAX_ROW" : 3,
 		}
 	};
 	
@@ -62,7 +169,7 @@ $(function(){
 		y : 1
 	};
 	
-	var map = [SETTINGS.MAP.MAX_X][SETTINGS.MAP.MAX_Y];
+	var map = [SETTINGS.MAP.MAX_COL][SETTINGS.MAP.MAX_ROW];
 	*/
     //renderMap(map, player);
     /*
@@ -83,14 +190,14 @@ $(function(){
 				}
 			break;
 			case KEY.s:
-				if(++player.y >= SETTINGS.MAP.MAX_Y){
+				if(++player.y >= SETTINGS.MAP.MAX_ROW){
 					player.y--;
 				}else{
 					renderMap(map, player);
 				}
 			break;
 			case KEY.d:
-				if(++player.x >= SETTINGS.MAP.MAX_X){
+				if(++player.x >= SETTINGS.MAP.MAX_COL){
 					player.x--;
 				}else{
 					renderMap(map, player);
@@ -118,11 +225,11 @@ function renderMap(map, player){
 
 
 function initMap(){
-	MAP = Array(SETTINGS.MAP.MAX_Y);
-	for( var i = 0 ; i < SETTINGS.MAP.MAX_Y ; i++ ){
-        MAP[i] = Array(SETTINGS.MAP.MAX_X);
-		for( var j = 0 ; j < SETTINGS.MAP.MAX_X ; j++ ){
-			MAP[i][j] = {
+	MAP = Array(SETTINGS.MAP.MAX_ROW);
+	for( var row = 0 ; row < SETTINGS.MAP.MAX_ROW ; row++ ){
+        MAP[row] = Array(SETTINGS.MAP.MAX_COL);
+		for( var col = 0 ; col < SETTINGS.MAP.MAX_COL ; col++ ){
+			MAP[row][col] = {
 				tile_layer_0	: "empty",
 				tile_layer_1	: "empty",
 				tile_layer_2	: "empty"
@@ -131,22 +238,22 @@ function initMap(){
     }
 	
 	redrawMap();
-	$('#player_map').width(SETTINGS.MAP.TILE_SIZE * SETTINGS.MAP.MAX_X);
-	$('#player_map').height(SETTINGS.MAP.TILE_SIZE * SETTINGS.MAP.MAX_Y);
+	$('#player_map').width(SETTINGS.MAP.TILE_SIZE * SETTINGS.MAP.MAX_COL);
+	$('#player_map').height(SETTINGS.MAP.TILE_SIZE * SETTINGS.MAP.MAX_ROW);
 	$('#player_map').css("margin", "0 auto");
 }
 
 function redrawMap(){
 	var out = '';
 
-    for( var i = 0 ; i < SETTINGS.MAP.MAX_Y ; i++ ){
+    for( var row = 0 ; row < SETTINGS.MAP.MAX_ROW ; row++ ){
         out += '<div class="row">';
-        for( var j = 0 ; j < SETTINGS.MAP.MAX_X ; j++ ){
+        for( var col = 0 ; col < SETTINGS.MAP.MAX_COL ; col++ ){
             out += '<div class="column">' +
-            "<div id="+i+":"+j+" class='" + 'empty' + "'>" + 
-			"<img src='images/" + MAP[i][j].tile_layer_0 + ".png' id='tile_layer_0' class='tile_container'></img>" + 
-			"<img src='images/" + MAP[i][j].tile_layer_1 + ".png' id='tile_layer_1' class='tile_container'></img>" + 
-			"<img src='images/" + MAP[i][j].tile_layer_2 + ".png' id='tile_layer_2' class='tile_container'></img>" + 
+            "<div id="+row+":"+col+" class='" + 'empty' + "'>" + 
+			"<img src='images/" + MAP[row][col].tile_layer_0 + ".png' id='tile_layer_0' class='tile_container'></img>" + 
+			"<img src='images/" + MAP[row][col].tile_layer_1 + ".png' id='tile_layer_1' class='tile_container'></img>" + 
+			"<img src='images/" + MAP[row][col].tile_layer_2 + ".png' id='tile_layer_2' class='tile_container'></img>" + 
 			"</div>" +
             '</div>';
         }
